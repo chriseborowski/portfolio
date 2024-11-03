1c70ccae11662a854839d75cd4a1be19202837b2
Chris Borowski <krzysztofeborowski@gmail.com>
2024-11-03 00:55:19 -0500

    Add expandText() function to support toggle view more feature

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function expandText() {
+  var text = document.getElementById("expand");
+  if (text.clientHeight) {
+    text.style.height = 0;
+  } else {
+    var wrapper = document.querySelector(".wrapper");
+    text.style.height = wrapper.clientHeight + "px";
+  }
+
+  document.getElementById("expand-button").value =
+    document.getElementById("expand-button").value == "Read more"
+      ? "Read more"
+      : "Read less";
+}
