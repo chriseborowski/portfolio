ae69f86370cb58c81a01688c7894b2a3ff0eb3b6
Chris Borowski <krzysztofeborowski@gmail.com>
2024-11-13 23:05:18 -0600

    Add expandText function to manage Read more button behavior

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-function expandText() {
-  var text = document.getElementById("expand");
-  if (text.clientHeight) {
-    text.style.height = 0;
-  } else {
-    var wrapper = document.querySelector(".wrapper");
-    text.style.height = wrapper.clientHeight + "px";
-  }
+let currentlyExpanded = null;
 
-  document.getElementById("expand-button").value =
-    document.getElementById("expand-button").value === "Read more"
-      ? "Read less"
-      : "Read more";
+function expandText(id) {
+  const text = document.getElementById("expand-" + id);
+  const button = document.getElementById("expand-button-" + id);
+
+  if (text.style.height && text.style.height !== "0px") {
+    text.style.height = "0px";
+    button.value = "Read more";
+    currentlyExpanded = null;
+  } else {
+    if (currentlyExpanded && currentlyExpanded !== id) {
+      const prevText = document.getElementById("expand-" + currentlyExpanded);
+      const prevButton = document.getElementById(
+        "expand-button-" + currentlyExpanded
+      );
+      prevText.style.height = "0px";
+      prevButton.value = "Read more";
+    }
+
+    const wrapper = document.querySelector(".wrapper-" + id);
+    text.style.height = wrapper.clientHeight + "px";
+    button.value = "Read less";
+    currentlyExpanded = id;
+  }
 }
